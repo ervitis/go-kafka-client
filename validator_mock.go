d5139e24fa9c4ff99a8f904426bc7397af5b63a6
ervitis <vitomarti@gmail.com>
2019-06-15 18:12:02 +0200

    feat: add validator mock

---
+++ validator_mock.go
@@ -0,0 +1,21 @@
+package go_kafka_client
+
+import "errors"
+
+type mockValidator struct{}
+
+func (m *mockValidator) ValidateData(msg []byte, schema schema) (bool, error) {
+	if schema.Value == "notvalid" {
+		return false, nil
+	} else if schema.Value == "error" {
+		return false, errors.New("ups")
+	}
+	return true, nil
+}
+
+func (m *mockValidator) IsReachable(schema schema) bool {
+	if schema.Value == "notreachable" {
+		return false
+	}
+	return true
+}
