67f65fb9282ec32b205925d89ecec22032a151be
ervitis <vitomarti@gmail.com>
2019-06-15 22:20:21 +0200

    fix: dependencies file for travis

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib/
+PATH=${PATH}:${PKG_CONFIG_PATH}:${LD_LIBRARY_PATH}
+
 git clone -b v1.0.1 https://github.com/edenhill/librdkafka
 cd librdkafka
 ./configure --install-deps
