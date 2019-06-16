0d1c988446e914101fb2fa4663eae74c654ac47c
ervitis <vitomarti@gmail.com>
2019-06-16 18:07:00 +0200

    debug travis

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
-LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib/
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib/:/usr/local/include/
 PATH=${PATH}:${PKG_CONFIG_PATH}:${LD_LIBRARY_PATH}
 
 git clone -b v1.0.1 https://github.com/edenhill/librdkafka
@@ -9,3 +9,5 @@ cd librdkafka
 ./configure --install-deps
 make
 sudo make install
+
+sudo ldconfig
